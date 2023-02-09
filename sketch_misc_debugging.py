ad856d6d3828637aeccacedb454ea3a08f73eca5
R4chel <rae84@cornell.edu>
2023-02-09 12:35:06 -0500

    add debugginb

--- sketch_misc_debugging.py
+++ sketch_misc_debugging.py
@@ -1,6 +1,6 @@
 import vsketch
 from shapely.geometry import Point
-
+import math
 
 class MiscDebuggingSketch(vsketch.SketchClass):
     # Sketch parameters:
@@ -8,20 +8,19 @@ class MiscDebuggingSketch(vsketch.SketchClass):
     width = vsketch.Param(5., decimals=2, unit="in")
     height = vsketch.Param(3., decimals=2, unit="in")
     pen_width = vsketch.Param(0.7, decimals=3, unit="mm")
+    radius = vsketch.Param(-1, decimals=3, unit="in")
     num_layers = vsketch.Param(1)
 
-    def random_point(self, vsk: vsketch.Vsketch):
-        return Point(vsk.random(0, self.width), vsk.random(0, self.height))
-
     def draw(self, vsk: vsketch.Vsketch) -> None:
-        vsk.size(f"{self.height}x{self.width}", landscape=True, center=False)
+        vsk.size(f"{self.height}x{self.width}", landscape=True, center=True)
         vsk.penWidth(f"{self.pen_width}")
 
-        layers = [1 + i for i in range(self.num_layers)]
         # implement your sketch here
-        # layer = layers[math.floor(vsk.random(0, len(layers)))]
-        # vsk.stroke(layer)
-        # vsk.circle(0, 0, self.radius, mode="radius")
+        layers = [1 + i for i in range(self.num_layers)]
+        layer = layers[math.floor(vsk.random(0, len(layers)))]
+        vsk.stroke(layer)
+        vsk.fill(layer)
+        vsk.circle(0, 0, self.radius, mode="radius")
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
